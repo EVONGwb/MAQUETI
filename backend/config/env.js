a87c192958af1d560521c37fbdb63d1923368385
Prudencio Madjoke mbula <evongorecords@MacBook-Air-de-Prudencio.local>
2026-03-22 21:41:51 +0100

    Use PORT env var

--- backend/config/env.js
+++ backend/config/env.js
@@ -1,7 +1,7 @@
 const env = { 
-  port: 3000, 
-  appName: "MAQUETI", 
-  mode: "development", 
+  port: process.env.PORT || 3000, 
+  appName: process.env.APP_NAME || "MAQUETI", 
+  mode: process.env.NODE_ENV || "development", 
 }; 
 
 module.exports = env; 
